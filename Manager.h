9e01f4e372b075e38e12cd588e69753db88db7e2
Rodrigo Povoa <up202108890@up.pt>
2023-01-07 22:43:09 +0000

    All documented in the manager class

--- Manager.h
+++ Manager.h
@@ -25,126 +25,143 @@ private:
     Graph graph;
 
 public:
+     //! An class constructor taking no arguments and doesn't return anything.
     Manager();
-     //! A normal method taking no arguments and doesn´t return anything.
+     //! An normal method taking no arguments and doesn't return anything.
      /*!
-         Reads the files them in their respective data structures.
+         Reads the files then put the data in their respective data structures.
          \Complexity  0(n)
+         \n Where the n is the number on lines (in this case the airlines) in the file.
      */
     void ReadAirLines();
-     //! A normal method taking no arguments and doesn´t return anything.
+     //! An void method taking no arguments and doesn't return anything.
      /*!
          Reads the files them in their respective data structures.
-         a Complexity é N o number of airports , E the number of airports int the city and S the number of cities in a Country
          \Complexity  0(N log(E) + N log(S))
+         \n
+           Where N is the number of airports, E the number of airports int the city and S the number of cities in a Country
      */
     void ReadAirports();
-     //! A normal method taking no arguments and doesn´t return anything.
+     //! An void method taking no arguments and doesn't return anything.
      /*!
          Reads the files them in their respective data structures.
          \Complexity  0(n)
+         \n Where the n is the number on lines (in this case the flights) in the file.
      */
     void ReadFlights();
-     //! A normal method taking The name of the city return the airports in the city
+     //! An List<String> method taking the name of the city and returns the airports in that city.
      /*!
-         This method utilizes unordered_map so is very efficient .
+         This method utilizes unordered_map so it's very efficient .
          \Complexity  0(1)
+         \Arguments @b String city
      */
     list<string> getAirportsInCity(string city);
-     //! A normal method taking The name of the country return the airports in the city
+     //! An set<string> method taking the name of the country and returns the cities in that country.
      /*!
-         This method utilizes unordered_map so is very efficient.
+         This method utilizes unordered_map so it's very efficient.
          \Complexity  0(1)
+         \Arguments @b String Country
      */
     set<string> getCitysInContry(string country);
-     //! A normal method taking The AirportCode that return the airport
+     //! An Airport method taking The airportCode and then returns the airport with that code.
      /*!
-         This method utilizes unordered_map so is very efficient.
+         This method utilizes unordered_map so it's very efficient.
          \Complexity  0(1)
+         \Arguments @b String airportCode
      */
     Airport getAirportWithCode(string airportCode);
-     //! A normal method taking The AirlineCode that return the airport
+     //! An Airline method taking The airlineCode that returns the airline with that code.
      /*!
          This method utilizes unordered_map so is very efficient.
          \Complexity  0(1)
+         \Arguments @b String airlineCode
      */
      Airline getAirlineWithCode(string airlineCode);
-     //! A normal method taking The AirportCode that return a integer
+     //! An integer method that takes the airportCode and returns the number of flights to that airport.
      /*!
-         This method returns the number of flights to a certain Airport , n is the number of flights.
-         \Complexity  0(n)
+         This method uses a iterator.
+         \Complexity  0(n) \n Where n is the number of flights.
+         \Arguments @b String airportCode
      */
     int getNumFlightTo(string aiportCode);
-     //! A normal method taking The AirportCode that return a integer
+     //! An integer method taking The airportCode and that returns the number of flights from that airport.
      /*!
-         This method returns the flights that begin in the Airport that the airport code is referring to
-         ,utilizes unordered_map so is very efficient.
+         This method utilizes unordered_map so it's very efficient.
          \Complexity  0(1)
+         \Arguments @b string airportCode
      */
     int getNumFlightFrom(string aiportCode);
-     //! A normal method taking The AirportCode that return a integer
+     //! An integer method taking the airportCode and returns the number of flights to a certain airport that has the airportCode and is referring to from different airlines.
      /*!
-         This method returns the number of flights to a certain airport that the airportCode is referring to from different airlines , n is the number of flights.
-         \Complexity  0(n)
+
+         \Complexity  0(n) \n Where the n is the number of flights
+         \Arguments @b String airportCode
+
      */
     int getNumFlightFromDiferentAirlinesTo(string aiportCode);
-     //! A normal method taking The AirportCode that return a integer
+     //! An integer method taking The AirportCode that returns the flights that begin in the Airport that the airportCode is referring to
      /*!
-         This method returns the flights that begin in the Airport that the airportCode is referring to
-         ,utilizes unordered_map so is very efficient.
+         This method utilizes the unordered_map so it's very efficient.
          \Complexity  0(1)
+         \Arguments @b String airportCode
      */
     int getNumFlightFromDiferentAirlinesFrom(string aiportCode);
-     //! A normal method taking The AirportCode that return a integer
+     //! An integer method taking The AirportCode that returns the number of flights to a certain airport that the airportCode is referring to from different Countries.
      /*!
-         This method returns the number of flights to a certain airport that the airportCode is referring to from different Countries, n is the number of flights.
-         \Complexity  0(nlog(n))
+         \Complexity  0(nlog(n)) \n Where n is the number of flights
+         \Arguments @b String airportCode
+
      */
     int getNumFlightFromDiferentCountrysTo(string aiportCode);
-     //! A normal method taking The AirportCode that return a integer
+     //! An integer method taking The AirportCode that returns the number of flights from a certain airport that the airportCode is referring to from different Countries
      /*!
-         This method returns the number of flights from a certain airport that the airportCode is referring to from different Countries , n is the number of flights.
-         \Complexity  0(log(n))
+         \Complexity  0(log(n)) \n Where the n is the number of flights
      */
     int getNumFlightToDiferentCountrysFrom(string aiportCode);
-     //! A normal method taking no arguments and return an unordered_map.
+     //! An unordered_map-string,Airline- method taking no arguments and returns an unordered_map, where the key is the AirlineCode.
      /*!
-         Returns an unordered_map where the key is the AirlineCode and Returns and Airline.
          \Complexity 0(1)
      */
      unordered_map <string,Airline>  getAirlines();
-     //! A normal method taking no arguments and return an unordered_map.
+     //! A unordered_map-string,Airline- method taking no arguments and returns an unordered_map where the key is the AirportCode.
      /*!
-         Returns an unordered_map where the key is the AirportCode and Returns and Airport.
          \Complexity 0(1)
      */
      unordered_map <string,Airport> getAirports();
-     //! A normal method taking a string source (AirportCode) and string Target (AirportCode).
+     //! A vector-Flight- method taking a string source (AirportCode) and string Target (AirportCode).
      /*!
-         Returns a path with the shortest length possible that goes from the airportCode source and airportCode target
-         \Complexity  O(V+E), where V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \n Returns a path with the shortest length possible that goes from the airportCode source and airportCode target
+         \Complexity  O(V+E) \n Where V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \Arguments @b String source
+         \n @b String target
      */
      vector<Flight> FindBestRoute(string source, string target);// da output de 1 caminho com a route mais pequena
-     //! A normal method taking a set and string source (AirportCode) and string Target (AirportCode).
+     //! A vector-Flight- method taking a set and string source (AirportCode) and string Target (AirportCode).
      /*!
-         Returns a path with the shortest length possible that goes from the airportCode source and airportCode target
-         With airlines blacklisted
+         \n Returns a path with the shortest length possible that goes from the airportCode source and airportCode target with blacklisted Airlines.
 
-         \Complexity  O(V+E) ,where V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \Complexity  O(V+E) \n Where V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \Arguments @b String source
+         \n @b String target
+         \n @b Set-string- blackListAirlines
      */
      vector<Flight> FindBestRouteWithBlackListed(string source, string target,set<string> blackListAirlines);//faz o mesmo que a anterior com black listed
-     //! A normal method taking a set and string source (AirportCode) and string Target (AirportCode).
+     //! A vector-Flight- method taking a set and string source (AirportCode) and string Target (AirportCode).
      /*!
          Returns a path with the shortest length possible that goes from the airportCode source and airportCode target
          With airlines GreenListed(only uses the airlines in the set green list)
-         \Complexity  O(V+E) , where V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \Complexity  O(V+E) \n Where V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \Arguments @b String source
+         \n @b String target
+         \n @b Set-string- greenListAirlines
      */
      vector<Flight> FindBestRouteWithGreenListed(string source, string target,set<string> greenListAirlines);//faz o mesmo que a anterior com white listed
-     //! A normal method taking a string source (AirportCode) and string Target (AirportCode).
+     //! A vector-Flight- method taking a string source (AirportCode) and string Target (AirportCode).
      /*!
          Returns all possible paths with the shortest length possible that goes from the airportCode source and airportCode target
-         \Complexity  O(R * (V+E)), where R is the number of routes found and V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
-
+         \Complexity  O(R * (V+E)) \n where R is the number of routes found and V is the number of vertices (airports) in the graph and E is the number of edges (flights) in the graph
+         \Arguments @b String source
+         \n @b String target
      */
      vector<vector<Flight>> FindBestRoutes1(string source, string target);//função calcula todas as routes entre os dois aeroportos
      //! This function is auxiliary to FindBestRoutes1.
@@ -154,36 +171,57 @@ public:
 
      */
      vector<vector<Flight>> FindBestRoutes(string source, string target,vector<vector<Flight>>& result);//função auxiliar a anterior
-     //! A normal method taking Coordinates and a int distance that returns a vector.
+     //! A vector-string- method taking Coordinates and a int distance that returns an vector.
      /*!
-         This method taking Coordinates and a int distance that returns a vector with the AirportCodes
-         with the distance of distanteSource from the coordinates
+         This method taking Coordinates and a int distance that returns a vector with the AirportCodes with the distance of distanteSource from the coordinates.
          \Complexity  0(n)
+         \Arguments @b Cordenadas cordenadas
+         \n @b int distanceSource
      */
      vector<string> FindAirportsFromCordenadas(Cordenadas cordenadas,int distanceSource);//Encontra todos os aeroportos a distancia de distanceSource da cordenada
      //! A normal method taking a Country Name.
      /*!
          This method taking Country returns a vector with all AirportCodes in the Country
-         \Complexity  0(n) , Where n is the number of airports in the country
+         \Complexity  0(n) \n Where n is the number of airports in the country
      */
      vector<string> FindAirportsFromCountry(string country);//Encontra todos os aeroportos no pais contry
-     //! A normal method taking a City Name.
+     //! A vector-string- method taking a City Name.
      /*!
-         This method taking City Name that returns a vector runs all AirportCodes
-         in the city
+         This method taking City Name that returns a vector with all AirportCodes in the city
          \Complexity  0(n) Where n is the number of airports in the city.
+         \Arguments @b string city
      */
      vector<string> FindAirportsFromCity(string city);//Encontra todos os aeroportos no cidade
-     //! A normal method taking a vector of strings starts and a vector with targets and returns a vector of vectors with strings.
+     //! A vector-flight- method taking a vector of strings starts and a vector with targets and returns a vector of vectors with strings.
      /*!
-         This method taking a vector of strings starts is made of AirportCodes and a vector with targets is made of AirportCodes
-         returns a route per combination of aeroport if they have the minimum length.
-         \Complexity   O(S * T * (V+E)) where S is the number of starting airports, T is the number of target airports,
+         This method returns a route per combination of airports if they have the minimum length.
+         \Complexity   O(S * T * (V+E)) \n  Where S is the number of starting airports, T is the number of target airports,
          V is the number of vertices (airports) in the graph, and E is the number of edges (flights) in the graph.
+         \Arguments @b vector-string- starts
+         \n @b vector-string- targets
      */
      vector<vector<Flight>> FindBestRoutesFromAirportsToAirports(vector<string> starts, vector<string> targets);//encontra entre os varios paises uma rota e da output as rotas com tamanho minimo
+     //! A integer method taking a  strings source and an integer n, where n is the number of flights and the source is the airportcode.
+     /*!
+         This method returns the number of countries that are reachable within n flights
+         \Complexity   O(n*m) \n  Where n is the number of flights allowed, m is the average number of countries reachable in y flights
+         \Arguments @b string src
+         \n @b int n
+     */
      int CountCountriesReachableInNFlights(const string& src, int n);//da output ao numero de paises que podes chegar de src em y fligths
+     //! A unordered_set-string- method taking a  strings source and an integer n, where y is the number of flights and the source is the airportcode.
+     /*!
+         This method returns the an set of countries that are reachable within y flights
+         \Complexity   O(n+m) \n  Where n is the number of airports, m is the number of flights.
+         \Arguments @b string src
+         \n @b int y
+     */
      unordered_set<string> CountriesReachableInYFlights(const string& src, int y);//da output aos paises que podes chegar de src em y fligths
+     //! A set-string- method taking no arguments.
+     /*!
+         This method returns the an set where when used with .size returns the number of the articulation points.
+         \Complexity   O(n+m) \n  Where n is the number of airports, m is the number of flights.
+     */
      set<string> findArticulationPoints();//find articulation Airports sao os quais se removeres em ves de um ciclo ficas com dois 
 
 
